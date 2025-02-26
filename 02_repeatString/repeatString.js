a15da804d21f7284cdce50247c72584aab0d3984
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-02-26 09:56:59 +0545

    Complete the exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringPassed, numberOfRepetition) {
+    let repeatedString = "";
 
+    if (numberOfRepetition < 0) {
+        return "ERROR";
+    }
+    
+    for (let i = 0; i < numberOfRepetition; i++) {
+        repeatedString += stringPassed;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
