cc8df289d4f35615a642959480827b29e7962ac7
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-02-26 16:36:29 +0545

    Complete the exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    let newArr = [];
+
+    arr.forEach((item) => {
+        if (!items.includes(item)) {
+            newArr.push(item);
+        }
+    });
+
+    return newArr;
 };
 
 // Do not edit below this line
