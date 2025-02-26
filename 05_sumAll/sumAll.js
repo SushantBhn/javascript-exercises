2bb0cb384caa1914998f85a52e3a1d654b373963
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-02-26 19:10:31 +0545

    Complete the exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(lowerRange, upperRange) {
+    if (lowerRange < 0 || upperRange < 0 || typeof lowerRange !== "number" || typeof upperRange !== "number" || !Number.isInteger(lowerRange) || !Number.isInteger(upperRange)) {
+        return "ERROR";
+    }
 
+    if (lowerRange > upperRange) {
+        let temp = lowerRange;
+        lowerRange = upperRange;
+        upperRange = temp;
+    }
+
+    let sum = 0;
+
+    for (let i = lowerRange; i <= upperRange; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
