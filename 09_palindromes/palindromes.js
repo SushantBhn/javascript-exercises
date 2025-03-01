a18c5ef2b69931f9051365016bca7aeb5f0bb11f
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-03-01 16:51:03 +0545

    Complete the exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let arr = str.split("");
+    //Filter out non-alphanumeric characters
+    arr = arr.filter((char) => {
+        if(char === "," || char === "!" || char === "." || char === " ") {
+            return false;
+        }
+        return true;
+    });
+    let originalArr = arr.slice(0);
+    let reversedArr = arr.reverse();
+    return originalArr.join("") === reversedArr.join("");
+}
 // Do not edit below this line
 module.exports = palindromes;
