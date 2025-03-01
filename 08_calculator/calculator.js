cf8f7723ea9141369a5b1fb34657bf96477e3cc6
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-03-01 15:23:49 +0545

    Complete the exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => {
+    return sum + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => {
+    return product * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let facto = 1;
+  for(let i = n; i > 0; i--) {
+    facto *= i;
+  }
+  return facto;
 };
 
 // Do not edit below this line
