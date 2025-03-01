5eed2f4dd268961550c412ea41f14699dbf22fe7
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-03-01 19:33:36 +0545

    Complete the exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //Find the age(current age or years lived, if dead) for each person
+    people.forEach((person) => {
+        if(person.yearOfDeath === undefined) {
+            const currentDate = new Date();
+            const currentYear = currentDate.getFullYear();
+            age = currentYear - person.yearOfBirth;
+            person.age = age;
+        } else {
+        age = person.yearOfDeath - person.yearOfBirth;
+        person.age = age;
+        }
+    });
+    let oldestPerson = {
+        name: "",
+        age: people[0].age
+    };
 
+    //Find the oldest person
+    people.forEach((person) => {
+        if(person.age >= oldestPerson.age) {
+            oldestPerson.name = person.name;
+            oldestPerson.age = person.age;
+        }
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
